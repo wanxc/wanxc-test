979841af31863c3040c5795307a23bf9ad9a33a5
wanxc <51305526+wanxc@users.noreply.github.com>
2022-04-29 16:43:38 +0800

    Update GitMapper.java

--- WanTest/src/main/java/com/wanxc/dao/GitMapper.java
+++ WanTest/src/main/java/com/wanxc/dao/GitMapper.java
@@ -4,4 +4,11 @@ public interface GitMapper {
     public void hello();
 
     public void hi();
+    
+    public void test11();
+    public void test21();
+    public void test31();
+    public void test41();
+    public void test51();
+    public void test61();
 }

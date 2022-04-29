ac8062822c19974ef072b36f571c78b16ed7d244
van <958423732@qq.com>
2022-04-29 16:44:06 +0800

    update GitMapper

--- WanTest/src/main/java/com/wanxc/dao/GitMapper.java
+++ WanTest/src/main/java/com/wanxc/dao/GitMapper.java
@@ -4,4 +4,11 @@ public interface GitMapper {
     public void hello();
 
     public void hi();
+
+    public void test1();
+    public void test2();
+    public void test3();
+    public void test4();
+    public void test5();
+    public void test6();
 }
